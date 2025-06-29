b1cb16e529f735d8f5ddb00c393d3e0ada56724d
Pranav Chinmay Aditya <154324689+Meleegod01@users.noreply.github.com>
2025-06-29 20:20:53 +0530

    Update EventTicketingSystem.sol

--- contracts/EventTicketingSystem.sol
+++ contracts/EventTicketingSystem.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+import {EventFactory} from "./EventFactory.sol";
+import {EventTicket} from "./EventTicket.sol";
+import {Marketplace} from "./Marketplace.sol";
+
+/**
+ * @title EventTicketingSystem
+ * @dev Deployment helper contract that deploys all components
+ */
+contract EventTicketingSystem {
+    EventFactory public immutable eventFactory;
+    Marketplace public immutable marketplace;
+    
+    event SystemDeployed(address indexed eventFactory, address indexed marketplace);
+    
+    constructor(uint256 baseFee, uint256 feePerTicket) {
+        eventFactory = new EventFactory(baseFee, feePerTicket);
+        marketplace = new Marketplace();
+        
+        emit SystemDeployed(address(eventFactory), address(marketplace));
+    }
+}
